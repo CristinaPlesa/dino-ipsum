6087e0dbd91b2a42ee3dc0f70b2812d2d965b167
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-20 14:35:54 -0700

    add class functionality with get request layout.
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,17 +2,35 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
 
-$(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-    debugger;
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3);
-    const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
-  });
-});
+class dinoIpsumGetter {
+  static getFromApi(numberOfParagraphs) {
+    try {
+      let promise = new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        const url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${numberOfParagraphs}`;
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(request.response);
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+      })
+
+      promise.then(function(response) {
+        const body = JSON.parse(response);
+        console.log(response);
+        displayIpsum(response.data);
+      }, function(error) {
+        throw Error(error)
+      })
+    } catch(error) {
+      console.error(error)
+    }
+  }
+}
+
+function displayIpsum()
